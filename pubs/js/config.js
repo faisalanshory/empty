53eb8d7eab46f170905c7fa9e0ca8b0a01ce2603
faisalanshory <faisalanshory.m@gmail.com>
2020-06-23 19:03:27 +0700

    new upd

---
+++ pubs/js/config.js
@@ -0,0 +1,19 @@
+var config = {
+  "title":"Kota Bandung",
+  "key":"16235db299c64efcab3843584fbac419",
+  "min_zoom": 5,
+  "home":{
+    "lat_lon":[-6.914744, 	107.609810],
+    "zoom":12
+  },
+  "zooms":[
+    {
+      "name": "Zoom Out",
+      "id": "EM",
+      "lat_lon": [-6.914744, 	107.609810],
+      "zoom": 12 
+    },
+  ],
+  "states_url":"./pubs/data/states.geojson",
+  "stats_url":"./pubs/data/stats.json"
+};
